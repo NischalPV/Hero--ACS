1725c76700d94925dd7ee66c283dae08571dee52
nischalvarasada <npvarasada@hotmail.com>
2021-06-10 09:04:48 +0530

    Removed compile files

--- HeroBot/ClientApp/src/containers/ChatScreen.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_redux_1 = require("react-redux");
-var ChatScreen_1 = require("../components/ChatScreen");
-var mapStateToProps = function (state) { return ({
-    isRemoved: state.threadMembers.isRemoved
-}); };
-exports.default = react_redux_1.connect(mapStateToProps)(ChatScreen_1.default);
-//# sourceMappingURL=ChatScreen.js.map
